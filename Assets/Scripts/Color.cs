5301da1536a665f710d7561fbf4803beadb436db
TheoTSP <lardeurgerszteintheo@gmail.com>
2022-12-06 12:36:20 +0100

    Scripts

---
+++ Assets/Scripts/Color.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Color : MonoBehaviour
+{
+
+    private new Renderer renderer;
+    private UnityEngine.Color initialColor;
+    public UnityEngine.Color color;
+
+
+
+    private void Awake()
+    {
+        /* Mise en cache du renderer pour éviter
+         * l'appel couteux de GetComponent dans l'Update
+         */
+        renderer = GetComponent<Renderer>();
+
+        // Mise en cache de la couleur initiale afin de pouvoir la restaurer
+        initialColor = renderer.material.color;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseEnter()
+    {
+        renderer.material.color = color;
+    }
+
+    private void OnMouseExit()
+    {
+        renderer.material.color = initialColor;
+    }
+
+    private float offset = 2f;
+    private void OnMouseDown()
+    {
+        GameObject go = Instantiate(
+            gameObject,
+            transform.position + Vector3.left * offset,
+            Quaternion.identity,
+            null);
+        Renderer goRenderer = go.GetComponent<Renderer>();
+        goRenderer.material.color = UnityEngine.Color.green;
+    }
+
+}
---
+++ Assets/Scripts/MovementManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementManager : MonoBehaviour
+{
+
+    public int speed;
+    public int dSpeed;
+    public int qSpeed;
+
+    public Space space;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * speed);
+        }
+
+        if(Input.GetKey(KeyCode.Q))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * dSpeed, space);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * qSpeed, space);
+        }
+
+    }
+}
